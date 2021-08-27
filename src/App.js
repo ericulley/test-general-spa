138b61236f28b31f4b3be87f256535d7ee32cfba
Eric Culley <eric.culley@auth0.com>
2021-08-27 15:44:17 -0600

    added userinfo endpoint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 // Dependencies
-import { Auth0Provider } from '@auth0/auth0-react'
+import { Auth0Provider, useAuth0 } from '@auth0/auth0-react'
 //Components
 import NavBar from './components/NavBar'
 import Main from './components/Main'
 import RequestData from './components/RequestData'
+import UserInfo from './components/UserInfo'
+
+const DOMAIN = process.env.REACT_APP_AUTH0_DOMAIN
+const CLIENT_ID = process.env.REACT_APP_AUTH0_CLIENT_ID
+const AUDIENCE = process.env.REACT_APP_AUTH0_AUDIENCE
 
 const App = () => {
 
+  const {  } = useAuth0()
+
   return (
     <Auth0Provider
-      domain={process.env.REACT_APP_AUTH0_DOMAIN}
-      clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+      domain={DOMAIN}
+      clientId={CLIENT_ID}
       redirectUri={window.location.origin}
       // Set audience to the identifier of the API you created
-      audience={process.env.REACT_APP_AUTH0_AUDIENCE}
+      audience={AUDIENCE}
+      // useRefreshTokens={true}
       scope="read:auth"
+      // sessionCheckExpiryDays is basically tryAutoAuthorize
+      // sessionCheckExpiryDays={.01}
     >
       <div className="App">
         <NavBar />
         <Main />
         <RequestData />
+        <UserInfo />
       </div>
     </Auth0Provider>
   )
---
+++ src/components/UserInfo.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react"
+import axios from "axios"
+import { useAuth0 } from "@auth0/auth0-react"
+
+const DOMAIN = process.env.REACT_APP_AUTH0_DOMAIN
+
+const UserInfo = () => {
+
+    const [userData, setUserData] = useState()
+
+    const { getAccessTokenSilently, getIdTokenClaims } = useAuth0()
+
+    const fetchUserData = async () => {
+        try {
+            const aToken = await getAccessTokenSilently()
+            const response = await axios.get(`https://${DOMAIN}/userinfo`, 
+            { 
+                headers: {
+                    Authorization: `Bearer ${aToken}`,
+                }
+            })
+            setUserData(response.data)
+            console.log(response.data)
+        } catch (err) {
+            console.error(err.message)
+        }
+    }
+
+    return (
+        <div id="auth-container">
+            <h3>/UserInfo Endpoint</h3>
+            <button onClick={fetchUserData}>/userinfo Endpoint</button>
+            {userData ? <p>Check Console</p> : <p>----</p>}
+        </div>
+    )
+}
+
+export default UserInfo
