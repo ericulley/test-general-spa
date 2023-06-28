0f95d85bdb159073e9309d76498a23f1f9929145
Eric Culley <eric.culley@auth0.com>
2023-06-28 16:57:11 -0600

    added org invite to default login route

--- src/pages/DefaultLogin.jsx
+++ src/pages/DefaultLogin.jsx
@@ -1,10 +1,26 @@
 import { useAuth0 } from '@auth0/auth0-react'
+import { useSearchParams } from 'react-router-dom';
 
 const DefaultLoginRoute = () => {
+
     const { loginWithRedirect } = useAuth0()
+    let [searchParams, setSearchParams] = useSearchParams();
+
+    const invitation = searchParams.get('invitation');
+    const organization = searchParams.get('organization');
     
-    const redirectToAuthorize = () => {
-        return loginWithRedirect()
+    const redirectToAuthorize = (params) => {
+        if (organization && invitation) {
+            console.table({organization: organization, invitation: invitation})
+            loginWithRedirect({
+                authorizationParams: {
+                    organization: organization,
+                    invitation: invitation    
+                }
+            })
+        } else {
+            return loginWithRedirect()
+        }
     }
     return redirectToAuthorize()
 }
