05833808668be43b7eec085dee17edc779febad4
Eric Culley <eric.culley@auth0.com>
2023-08-13 16:08:13 -0600

    added idp-init login endpoint

--- src/pages/IdpLogin.jsx
+++ src/pages/IdpLogin.jsx
@@ -1,12 +1,23 @@
-import {  } from '@auth0/auth0-react'
+import { useAuth0 } from '@auth0/auth0-react'
+import { useSearchParams } from 'react-router-dom'
 
 const IdpLogin = () => {
 
-    return (
-        <div id="idp-login">
-            <h1>Initializing Login from Identity Provider</h1>
-        </div>
-    )
+    const { loginWithRedirect } = useAuth0()
+    let [searchParams] = useSearchParams();
+
+    const redirectToAuthorize = () => {
+        const connection = searchParams.get('connection');
+        if (connection) {
+            return loginWithRedirect({
+                    connection: connection,    
+            })
+        } else {
+            return loginWithRedirect()
+        }
+    }
+
+    return redirectToAuthorize()
 }
 
-export default (IdpLogin, {onRedirecting: () => (<div>Redirecting you to the login page...</div>)})
+export default IdpLogin
